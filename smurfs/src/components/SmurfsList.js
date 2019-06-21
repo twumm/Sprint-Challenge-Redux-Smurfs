189873169190891c284ef2cf1b020df898c56241
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-21 08:39:56 +0000

    feature: On state load, fetch smurfs Create a new Smurf component for each smurf item

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -2,17 +2,35 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux';
 
 import { fetchSmurfs } from '../actions'
+import Smurf from './Smurf';
 
-export function SmurfsList() {
+export function SmurfsList(props) {
+  const { fetchSmurfs, smurfs } = props;
+
   useEffect(() => {
-
-  })
+    fetchSmurfs();
+  }, fetchSmurfs)
 
   return (
     <div>
-      
+      {
+        smurfs.map(smurf => (
+          <Smurf
+            key={smurf.id}
+            name={smurf.name}
+            age={smurf.age}
+            height={smurf.height}
+          />
+        ))
+      }
     </div>
   )
 }
 
-export default connect(null, { fetchSmurfs })(SmurfsList);
+function mapStateToProps(state) {
+  return {
+    smurfs: state.smurfs
+  }
+}
+
+export default connect(mapStateToProps, { fetchSmurfs })(SmurfsList);
