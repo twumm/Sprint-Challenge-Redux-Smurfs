76d18861d4314e9b5a014d7c5192f61a11f43b81
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-21 09:23:56 +0000

    feature: Add button to delete smurf Pass down removeSmurf props Create onRemoveSmurf method to trigger removeSmurf

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-export default function Smurf(props) {
+export default function Smurf({ smurf, removeSmurf }) {
+  const onRemoveSmurf = () => {
+    removeSmurf(smurf.id)
+  }
+
   return (
     <div>
-      <h4>{props.name}</h4>
-      <p>{props.age}</p>
-      <h4>{props.height}</h4>
+      <h4>{smurf.name}</h4>
+      <p>{smurf.age}</p>
+      <h4>{smurf.height}</h4>
+      <button onClick={() => onRemoveSmurf()}>Delete</button>
     </div>
   )
 }
