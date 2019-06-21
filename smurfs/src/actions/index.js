194fa77b40398711fd60f564a9f9da820f9e6c43
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-21 08:06:25 +0000

    feature: Create actions to fetch and add smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,51 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+
+const smurfsApiURL = 'http://localhost:3333/smurfs';
+
 export const GET_SMURFS = 'GET_SMURFS';
 export const ADD_SMURF = 'ADD_SMURF';
-export const FETCHING_SMURF = 'FETCHING_SMURF';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
 export const ADDING_SMURF = 'ADDING_SMURF';
 export const UPDATING_SMURF = 'UPDATING_SMURF';
 export const DELETING_SMURF = 'DELETING_SMURF';
 export const FAILURE = 'FAILURE';
 
+export const getSmurfs = (smurfs) => {
+  return {
+    type: GET_SMURFS,
+    payload: smurfs,
+  }
+}
+
+export const genericAction = (type, payload) => {
+  return {
+    type,
+    payload
+  }
+}
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch(genericAction(FETCHING_SMURFS, true));
+  axios.get(smurfsApiURL)
+    .then(response => dispatch(getSmurfs(response.data)))
+    .catch(error => dispatch(genericAction(FAILURE, error)))
+    .finally(() => dispatch(genericAction(FETCHING_SMURFS, false)))
+}
+
+export const addSmurf = (smurf) => dispatch => {
+  dispatch(genericAction(ADDING_SMURF, true))
+  axios.post(smurfsApiURL, (smurf.name, smurf.age, smurf.height))
+    .then(response => dispatch(getSmurfs(response.data)))
+    .catch(error => dispatch(genericAction(FAILURE, error)))
+    .finally(() => dispatch(genericAction(ADDING_SMURF, false)))
+}
+
+
+
+/* 
+  Action Types Go Here!
+  Be sure to export each action type so you can pull it into your reducer
+*/
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
