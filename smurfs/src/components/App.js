227ece8e45520793c42cb52a1c3060d1b7e5e8ce
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-21 08:42:42 +0000

    feature: Import and add AddSmurf component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SmurfsList from './SmurfsList';
+import AddSmurf from './AddSmurf';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <SmurfsList />
+        <AddSmurf />
       </div>
     );
   }
