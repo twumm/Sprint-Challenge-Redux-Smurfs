9eb42cc3cb1e187d57f4c282807ab21e33ebd6f6
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-21 08:40:26 +0000

    feature: Create a form for adding a smurf

---
+++ smurfs/src/components/AddSmurf.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function AddSmurf() {
+  const smurfNameRef = React.createRef();
+  const smurfAgeRef = React.createRef();
+  const smurfHeightRef = React.createRef();
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter name"
+          ref={smurfNameRef}
+        />
+        <input
+          type="number"
+          placeholder="Enter a huge age"
+          ref={smurfAgeRef}
+        />
+        <input
+          type="number"
+          placeholder="How tall is this smurf?"
+          ref={smurfHeightRef}
+        />
+      </form>
+    </div>
+  )
+}
